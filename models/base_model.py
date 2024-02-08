3ee46483f7b9ce788200209f80e14faa3c26d3c6
SeM2x <houssem135.302@gmail.com>
2024-02-08 14:49:22 +0100

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -1,2 +1,43 @@
 #!/usr/bin/python3
+""" """
+import uuid
+from datetime import datetime
 
+
+class BaseModel:
+    """
+    """
+    def __init__(self, *args, **kwargs):
+        """
+        """
+        date_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value, date_format)
+                    setattr(self, key, value)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        """
+        new_dict = self.__dict__
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
+        return new_dict
