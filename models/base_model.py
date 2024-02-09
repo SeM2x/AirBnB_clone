34bb03f9d2ab1e42508f86cb6c6634a28f3db532
SeM2x <houssem135.302@gmail.com>
2024-02-09 09:31:59 +0100

    FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """ """
 import uuid
 from datetime import datetime
+from . import storage
 
 
 class BaseModel:
@@ -22,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self.to_dict())
 
     def __str__(self):
         """
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """
         """
+        storage.save()
         self.updated_at = datetime.now()
 
     def to_dict(self):
---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" """
+import json
+
+
+class FileStorage:
+    """
+    """
+    def __init__(self, path="file.json"):
+        """
+        """
+        self.__file_path = path
+        self.__objects = {}
+
+    def all(self):
+        """
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        """
+        self.__objects[f"{obj['__class__']}.{obj['id']}"] = obj
+
+    def save(self):
+        """
+        """
+        json_str = json.dumps(self.__objects)
+        with open(self.__file_path, 'w') as f:
+            f.write(json_str)
+    
+    def reload(self):
+        """
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                json_str = f.read()
+                self.__objects = json.loads(json_str)
+        except FileNotFoundError:
+            pass
