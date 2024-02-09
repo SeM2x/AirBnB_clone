9edcdf3f6dd9cefa7f0eb46b8e85e0c94f827ac9
SeM2x <houssem135.302@gmail.com>
2024-02-09 09:52:05 +0100

    console 0.0.1

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" """
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    """
+    prompt = "(hbnb) "
+    
+    def do_EOF(self, arg):
+        """EOF command to exit the program
+        """
+        return True
+    def do_quit(self, arg):
+        """Quit command to exit the program
+        """
+        return True
+    def emptyline(self):
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
